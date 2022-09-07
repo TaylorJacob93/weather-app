4cf94803b300567586e72675e4654b4cda357052
Jacob Taylor <jjakettaylor1993@hotmail.com>
2022-09-07 21:13:44 +0100

    created the base for the weather app project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="prop">
+      <h1>Weather App</h1>
+      <h2>learn react</h2>
+    </div>
+  );
+}
+
+export default App;
